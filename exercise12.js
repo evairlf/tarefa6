e2c41e87475d66750c80b3ac9023c59bb3dae501
Evairlf <evairleitefeldmann@gmail.com>
2024-04-28 20:11:33 -0300

    Consertado erro de lógica

--- exercise12.js
+++ exercise12.js
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')()
 
 for(let numero = 1000 ; numero<=1990 ; numero++){
-    if((numero*11)%5===0){
+    if(numero % 11===5){
         console.log(numero);
     }
 }
